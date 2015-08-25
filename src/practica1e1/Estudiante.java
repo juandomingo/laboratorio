8441a4859e463b197b7422e78e80bcaed180515a
tinoliam <tinoliam@gmail.com>
2015-08-25 10:40:05 -0300

    Primer commit

---
+++ src/practica1e1/Estudiante.java
@@ -0,0 +1,25 @@
+package practica1e1;
+
+public class Estudiante {
+	String nombre;
+	String apellido;
+	int legajo;
+	
+	
+	public Estudiante(String nombre, String apellido, int legajo){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.legajo = legajo;		
+	}
+	/*
+	public String toString()
+	{
+		StringBuffer strBuff = new StringBuffer(this.nombre);
+		strBuff.append(" ");
+		strBuff.append(this.apellido);
+		strBuff.append(" ");
+		return strBuff.append(this.legajo).toString();
+	}
+	*/
+	
+}
---
+++ src/practica1e1/TestEstudiante.java
@@ -0,0 +1,23 @@
+package practica1e1;
+
+public class TestEstudiante {
+	public static void main(String[] args)
+	{
+		//Estudiante[] estudiantes = new Estudiante[5];
+		Estudiante[] estudiantes = {new Estudiante("Antonio","Banderas",114404),
+				new Estudiante("Marco","Antonio",104404),
+				new Estudiante("Jose","García",10004),
+				new Estudiante("Gabriel","Saer",114404),
+				new Estudiante("Lykke","Li",114404)
+		};
+		
+		for (Estudiante estudiante : estudiantes) {
+			System.out.print(estudiante.toString() + "   ");
+			
+		}
+		if (new Estudiante("AAAAAA","AAAAAA",104404) == new Estudiante("AAAAAA","AAAAAA",104404))
+			System.out.print(" == mismos da ok   ");
+		if (new Estudiante("BBBBBB","BBBBBB",104404) == new Estudiante("XXXXXX","XXXXXX",104404))
+			System.out.print(" == dist da ok   ");
+	}
+}
